321e1f51057e7c721b4890ab266d1cc5896915fd
Alejandro23002553 <jose.madrid@galileo.edu>
2025-03-24 21:36:12 +0000

    first commit

---
+++ actividad3.php
@@ -0,0 +1,9 @@
+
+<?php   
+    $variable_nombre = "Alejandro Rodriguez Madrid";
+    echo "Hola mundo, soy " . $variable_nombre;
+
+
+?>
+<br/>
+<img src="/Actividad3_SeccionA/proyecto_php/perro.jpeg" >
